4437271b80c5ee003783622e0a18cfbbedbc4874
kumami-tech <sk924731@gmail.com>
2020-07-23 21:35:45 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # has_many :items, dependent: :destroy
+  # has_many :cards, dependent: :destroy
+  # has_one :address, dependent: :destroy
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :birth_day, presence: true
+  validates :nickname, presence: true
+
 end
