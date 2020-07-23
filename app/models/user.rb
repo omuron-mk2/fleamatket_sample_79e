dd9aa195ab6085e9119f6f0c6db58085a553d847
kumami-tech <sk924731@gmail.com>
2020-07-23 21:48:53 +0900

    add character type

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User < ApplicationRecord
   # has_many :cards, dependent: :destroy
   # has_one :address, dependent: :destroy
 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birth_year, presence: true
   validates :birth_month, presence: true
   validates :birth_day, presence: true
-  validates :nickname, presence: true
 
 end
