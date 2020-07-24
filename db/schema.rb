8008b43c129e39f7671a31ea7dda39f3ea60eef7
kumami-tech <sk924731@gmail.com>
2020-07-24 17:00:26 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_125411) do
+ActiveRecord::Schema.define(version: 2020_07_24_023635) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "send_last_name", null: false
