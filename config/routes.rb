099810b816f6b6917e000af19b77083f38b7a3d3
kumami-tech <sk924731@gmail.com>
2020-07-24 14:00:57 +0900

    user_test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'items#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
