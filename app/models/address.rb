b2ee148a7a43f8b36e905f46a30a0586e813bd1c
kumami-tech <sk924731@gmail.com>
2020-07-27 15:56:11 +0900

    add address validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,9 +8,9 @@ class Address < ApplicationRecord
   validates :send_first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :send_last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :send_first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: {with: //\A\d{3}[-]\d{4}\z/}
   validates :prefecture_id, presence: true
-  validates :city, presence: true
-  validates :house_number, presence: true
+  validates :city, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :house_number, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
 
 end
