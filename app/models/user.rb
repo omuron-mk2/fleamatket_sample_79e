a0337465c2a13c046058c79107f4ea51da553fd9
kumami-tech <sk924731@gmail.com>
2020-07-27 16:01:30 +0900

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,14 @@ class User < ApplicationRecord
   # has_many :cards, dependent: :destroy
 
   validates :nickname, presence: true
+  validates :email, presence: true, format: {with: /\A[a-zA-Z0-9]+\z/}
+  validates :password, presence: true, format: {with: /\A[a-zA-Z0-9]+\z/}
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :birth_year, presence: true
-  validates :birth_month, presence: true
-  validates :birth_day, presence: true
+  validates :birth_year, presence: true, format: {with: /\A[0-9]+\z/}
+  validates :birth_month, presence: true, format: {with: /\A[0-9]+\z/}
+  validates :birth_day, presence: true, format: {with: /\A[0-9]+\z/}
 
 end
