52aec754d6aca9bfa0bc4ca381370e544c51fb7e
kumami-tech <sk924731@gmail.com>
2020-07-28 22:31:45 +0900

    change layout

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  # before_action :no_layout_action
+
+  layout 'no_header_footer'
 
   def new
     @user = User.new
