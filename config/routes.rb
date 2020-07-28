502f7d3e4dd8c2ab7b7abedd5b059e71fcef4e2c
kumami-tech <sk924731@gmail.com>
2020-07-28 21:53:30 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
   end
-  root 'items#index'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'items#index'
 end
