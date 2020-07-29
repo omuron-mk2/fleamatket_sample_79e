42f820f9db751a01732f94a69fe2a4a9b6372eb5
kumami-tech <sk924731@gmail.com>
2020-07-29 14:52:30 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
@@ -12,5 +11,6 @@ Rails.application.routes.draw do
   root 'items#index'
 
   resources :cards 
+  resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
