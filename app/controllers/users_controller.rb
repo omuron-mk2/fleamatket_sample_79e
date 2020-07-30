2aa8bbbe489309037158989e9ded57df25d7b892
kumami-tech <sk924731@gmail.com>
2020-07-30 14:20:03 +0900

    create mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(current_user.id)
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :create
+    end
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    if current_user.update!(user_params)
+      redirect_to root_path
+      # redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :profile_image, :profile_text)
   end
 end
