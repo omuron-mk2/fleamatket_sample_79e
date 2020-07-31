679ca21657f82bb86778164a1abfb418dcf6e14c
kumami-tech <sk924731@gmail.com>
2020-07-31 11:48:57 +0900

    add index of user's exhibition and purchase

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,19 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
+    get 'users/addresses/:id/edit', to: 'users/registrations#edit_address'
+    patch 'users/addresses/:id', to: 'users/registrations#update_address'
   end
   root 'items#index'
 
   resources :cards 
+
   resources :users, only: [:show, :create, :edit, :update]
+  get '/users/:id/exhibition', to: 'users#exhibition'
+  get '/users/:id/purchase', to: 'users#purchase'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
