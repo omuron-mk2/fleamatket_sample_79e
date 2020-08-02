3cf47598f02faf06856940be93d29f2ae0994fe9
amagramming <p.mcr.s.lives@gmail.com>
2020-08-02 15:27:53 +0900

    itemDetail-page1

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   resources :cards 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'details', to: 'details#show'
 end
