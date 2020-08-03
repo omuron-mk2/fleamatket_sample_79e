b2e3a021015efe822cb4875a137fe967426bfc7d
masanorire0714 <masanorire0714@gmail.com>
2020-08-03 15:41:16 +0900

    test2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock '3.14.1'
 
+set :stage, :production
+
 set :application, "fleamatket_sample_79e"
 set :repo_url, "git@github.com:masanorire0714/fleamatket_sample_79e.git"
 
