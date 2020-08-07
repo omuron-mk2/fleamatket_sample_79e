73364541577b2c0f607955a369cbfd7374c5f70b
masanorire0714 <masanorire0714@gmail.com>
2020-08-07 15:58:53 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :basic_auth
+  before_action :basic_auth
 
   private
   def configure_permitted_parameters
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] && password == Rails.application.credentials[:basic_auth][:pass]
+      username == Rails.application.credentials.basic_auth[:user] && password == Rails.application.credentials.basic_auth[:pass]
     end
   end
 end
